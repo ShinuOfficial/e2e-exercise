32e1e67e2653b560bad98e2044871787ac5355b3
Shinu Mathew <shinu.mathew@samsung.com>
2020-11-03 20:06:18 +0530

    First Commit

---
+++ src/main/java/com/e2e/exercises/exercise1/BaseTest.java
@@ -0,0 +1,25 @@
+package com.e2e.exercises.exercise1;
+
+import org.testng.annotations.Test;
+
+public class BaseTest {
+
+	@Test
+	public void test1() {
+		/**
+		 * Create the below JSON object using any library.
+		 * Print the same in the console and also save the object in a file in src/main/resources.
+		 * Assert the created JSON is as expected.
+		 * */
+		
+	}
+
+	@Test
+	public void test2() {
+		/**
+		 * Read the JSON object from the file and deserialize it into a Java object
+		 * Assert the deserialized object
+		 * */
+		
+	}
+}
