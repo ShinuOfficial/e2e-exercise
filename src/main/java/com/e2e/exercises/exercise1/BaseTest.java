6fcf49b45b39f88a9e7cdf046f30e159843eef32
Sharathjayakumar <35330546+Sharathjayakumar@users.noreply.github.com>
2020-11-30 13:48:28 +0530

    Created JSON Object using GSON library

--- src/main/java/com/e2e/exercises/exercise1/BaseTest.java
+++ src/main/java/com/e2e/exercises/exercise1/BaseTest.java
@@ -1,20 +1,58 @@
 package com.e2e.exercises.exercise1;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 import org.testng.annotations.Test;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 
 public class BaseTest {
 
-	@Test
-	public void test1() {
-		
-		/** 
-		 * FIRST TEST COMES HERE:
-		 * 
-		 * Create the below JSON object using any library.
-		 * Print the same in the console and also save the object in a file in src/main/resources.
-		 * Assert the created JSON is as expected.
-		 * */
+	public static Gson gson;
 
+
+	@Test
+	public void test1() throws IOException {
+		
+		//Constructing a JSON Object
+		
+		JsonObject 	obj = new JsonObject();
+		obj.addProperty("device-code", "HZ096YKM");
+		obj.addProperty("device-name", "JBL T160BT Bluetooth Headset");
+		obj.addProperty("device-info", "JBL T160BT Bluetooth Headset - Black, In the Ear");
+		obj.addProperty("stock", "Instock");
+		obj.addProperty("serviceable", true);
+		obj.addProperty("available-quantities", 100);
+		
+		//Constructing a JSON array
+		
+		JsonArray colorCodes = new JsonArray();
+		colorCodes.add("red");
+		colorCodes.add("black");
+		colorCodes.add("white");
+	
+		obj.add("colorCodes", colorCodes);
+		
+		//Serializing & prettyPrinting it in a console
+		GsonBuilder gb = new GsonBuilder();
+		gb.setPrettyPrinting();
+		
+		gson = gb.create();
+		String deviceDetails = gson.toJson(obj);
+		
+		//gb.create().toJson(obj);
+		System.out.println(deviceDetails);
+
+		//Saving the object in src/main/resources
+		FileWriter jsonFile = new FileWriter(System.getProperty("user.dir") + "/src//main/resources/outputFile.json");
+		jsonFile.write(deviceDetails);
+		jsonFile.close();
+	
+		
 	}
 
 	@Test
